431ef6f8200f5678fbb8e285b7fa46160e478cd1
Danila Krohmalev <danila31rusk@ya.ru>
2024-08-14 20:12:21 +0300

    feat: Сonnect the model

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -9,17 +9,26 @@ export default class EventsPresenter {
   editPointComponent = new EditPointView();
   sortComponent = new SortView();
 
-  constructor({ container }) {
+  constructor({ container, pointsModel }) {
     this.container = container;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.eventPoints = [...this.pointsModel.getPoints()];
+    this.eventDestinations = [...this.pointsModel.getDestinations()];
+    this.eventOffers = [...this.pointsModel.getOffers()];
+
     render(this.sortComponent, this.container);
     render(this.editPointComponent, this.container);
     render(this.eventsListComponent, this.container);
 
-    for (let i = 0; i < 3; i++) {
-      render(new PointView(), this.eventsListComponent.getElement());
+    for (let i = 0; i < this.eventPoints.length; i++) {
+      render(new PointView(
+        {point: this.eventPoints[i]},
+        {destinations: this.eventDestinations[i]},
+        {offers: this.eventOffers[i]}
+      ),this.eventsListComponent.getElement());
     }
   }
 }
