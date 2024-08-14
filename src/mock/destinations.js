280af9f8bcf304a3d5d5f427ff381e4a19cf4595
Danila Krohmalev <danila31rusk@ya.ru>
2024-08-14 19:19:06 +0300

    feat: Add mock data and add the model to the application

---
+++ src/mock/destinations.js
@@ -0,0 +1,40 @@
+const mockDestinations = [
+  {
+    id: 'moscow-point-1',
+    description: 'Moscow is the capital of Russia',
+    name: 'Moscow',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163311',
+        description: 'Moscow Kremlin'
+      }
+    ]
+  },
+  {
+    id: 'saint-petersburg-point-2',
+    description: 'Saint Petersburg is a beautiful city',
+    name: 'Saint Petersburg',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Hermitage'
+      },
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163316',
+        description: 'Cruiser Aurora'
+      },
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163313',
+        description: 'Palace Square'
+      }
+    ]
+  },
+  {
+    id: 'belgorod-point-1',
+    description: '',
+    name: 'Belgorod',
+    pictures: []
+  }
+];
+
+export { mockDestinations };
