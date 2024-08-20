bcbe6860192e18a5297405d9d1e6d3643d89c6c9
Danila Krohmalev <danila31rusk@ya.ru>
2024-08-21 00:20:55 +0300

    feat: Select display points to function - Got rid of the code that immediately renders the editing form in the list.

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -1,5 +1,5 @@
 import { render } from '../framework/render.js';
-import EditPointView from '../view/edit-point-view.js';
+// import EditPointView from '../view/edit-point-view.js';
 import EventListView from '../view/event-list-view.js';
 import PointView from '../view/point-view.js';
 import SortView from '../view/sort-view.js';
@@ -26,19 +26,20 @@ export default class EventsPresenter {
     this.#eventDestinations = this.#pointsModel.destinations;
 
     render(this.#sortComponent, this.#eventsContainer);
-    render(new EditPointView({
-      point: this.#eventPoints[0],
-      offers: this.#eventOffers,
-      destinations: this.#eventDestinations
-    }), this.#eventsContainer);
+    // render(new EditPointView({
+    //   point: this.#eventPoints[0],
+    //   offers: this.#eventOffers,
+    //   destinations: this.#eventDestinations
+    // }), this.#eventsContainer);
 
     render(this.#eventListComponent, this.#eventsContainer);
     for (let i = 0; i < this.#eventPoints.length; i++) {
-      render(new PointView({
-        point: this.#eventPoints[i],
-        offers: this.#eventOffers,
-        destinations: this.#eventDestinations
-      }), this.#eventListComponent.element);
+      this.#renderPoint(this.#eventPoints[i], this.#eventOffers, this.#eventDestinations);
     }
+  }
+
+  #renderPoint(point, offers, destinations) {
+    const pointComponent = new PointView({ point, offers, destinations });
+    render(pointComponent, this.#eventListComponent.element);
   }
 }
